a333280ec17eedc423d51cde249ac604d1af59af
ArmGrl <michelle@michelles-macbook-pro.local>
2017-08-24 15:01:55 -0400

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by ArmGrl on 8/24/17.
+//  Copyright © 2017 ArmGrl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
